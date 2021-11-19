72383195d1bfbcfa2dc993d9ea1b5cd6c1c3f4f7
Alvaro Longueira <alvarolco@ext.inditex.com>
2021-11-19 08:57:00 +0100

    More tests

--- src/test/java/com/alvarolongueira/mowermanager/domain/PositionTest.java
+++ src/test/java/com/alvarolongueira/mowermanager/domain/PositionTest.java
@@ -1,14 +1,39 @@
 package com.alvarolongueira.mowermanager.domain;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PositionTest {
 
 	@Test
 	public void createPosition() {
-
+		Position position = new Position(4, 3, 8, 8);
+		Assert.assertEquals(4,position.getCurrentX());
+		Assert.assertEquals(3,position.getCurrentY());
+		Assert.assertEquals(8,position.getMaxX());
+		Assert.assertEquals(8,position.getMaxY());
 	}
 
+	@Test
+	public void createInvalidPositionOverXLimit() {
+		Position position = new Position(10, 6, 8, 8);
+	}
+
+	@Test
+	public void createInvalidPositionOverYLimit() {
+		Position position = new Position(2, 11, 8, 8);
+	}
+
+	@Test
+	public void createInvalidPositionUnderZeroX() {
+		Position position = new Position(10, 6, 8, 8);
+	}
+
+	@Test
+	public void createInvalidPositioUnderZeroY() {
+		Position position = new Position(2, 11, 8, 8);
+	}
+	
 	@Test
 	public void movePositionOneNorth() {
 
@@ -29,4 +54,14 @@ public class PositionTest {
 
 	}
 
+	@Test
+	public void moveSeveralWays() {
+
+	}
+	
+	@Test
+	public void moveOverLimitsStayInside() {
+
+	}
+	
 }
