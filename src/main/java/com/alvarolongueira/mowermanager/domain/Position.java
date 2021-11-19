47b77df79e99ad289d0e24f9aef5582039614223
Alvaro Longueira <alvarolco@ext.inditex.com>
2021-11-19 14:08:18 +0100

    Refactor name method

--- src/main/java/com/alvarolongueira/mowermanager/domain/Position.java
+++ src/main/java/com/alvarolongueira/mowermanager/domain/Position.java
@@ -11,39 +11,46 @@ public class Position {
 
 	private int currentX;
 	private int currentY;
-	private int maxX;
-	private int maxY;
+	private int max_X;
+	private int max_Y;
 
-	private Position(int currentX, int currentY, int maxX, int maxY) {
+	private Position(int currentX, int currentY, int max_X, int max_Y) {
 		this.currentX = currentX;
 		this.currentY = currentY;
-		this.maxX = maxX;
-		this.maxY = maxY;
+		this.max_X = max_X;
+		this.max_Y = max_Y;
 	}
 
+	private Position(int currentX, int currentY) {
+		this.currentX = currentX;
+		this.currentY = currentY;
+		this.max_X = currentX;
+		this.max_Y = currentY;
+	}
+	
 	static class Builder {
 
 		Position build() {
-			if (this.currentX < 0 || this.currentX > maxX) {
-				throw new IllegalArgumentException("X out of range [0 to " + maxX + "]: " + currentX);
+			if (this.currentX < 0 || this.currentX > max_X) {
+				throw new IllegalArgumentException("X out of range [0 to " + max_X + "]: " + currentX);
 			}
-			if (this.currentY < 0 || this.currentY > maxY) {
-				throw new IllegalArgumentException("X out of range [0 to " + maxY + "]: " + currentY);
+			if (this.currentY < 0 || this.currentY > max_Y) {
+				throw new IllegalArgumentException("X out of range [0 to " + max_Y + "]: " + currentY);
 			}
-			return new Position(currentX, currentY, maxX, maxY);
+			return new Position(currentX, currentY, max_X, max_Y);
 		}
 	}
 
-	public static Position of(int currentX, int currentY, int maxX, int maxY) {
-		return Position.builder().currentX(currentX).currentY(currentY).maxX(maxX).maxY(maxY).build();
+	public static Position of(int currentX, int currentY, int max_X, int max_Y) {
+		return Position.builder().currentX(currentX).currentY(currentY).max_X(max_X).max_Y(max_Y).build();
 	}
 
 	public void apply(Movement movement) {
 		this.currentX += movement.getX();
 		this.currentY += movement.getY();
 
-		this.currentX = this.keepMinMax(this.currentX, this.maxX);
-		this.currentY = this.keepMinMax(this.currentY, this.maxY);
+		this.currentX = this.keepMinMax(this.currentX, this.max_X);
+		this.currentY = this.keepMinMax(this.currentY, this.max_Y);
 	}
 
 	private int keepMinMax(int current, int max) {
