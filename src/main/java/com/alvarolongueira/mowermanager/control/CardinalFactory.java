122d4c980b708e1005a870361f4121787713e77c
Alvaro Longueira <alvarolco@ext.inditex.com>
2021-11-19 10:27:25 +0100

    Added test to CardinalFactory

--- src/main/java/com/alvarolongueira/mowermanager/control/CardinalFactory.java
+++ src/main/java/com/alvarolongueira/mowermanager/control/CardinalFactory.java
@@ -1,6 +1,26 @@
 package com.alvarolongueira.mowermanager.control;
 
+import com.alvarolongueira.mowermanager.domain.Cardinal;
 
 public class CardinalFactory {
 
+	private Cardinal current;
+
+	public static CardinalFactory of(Cardinal cardinal) {
+		return new CardinalFactory(cardinal);
+	}
+
+	private CardinalFactory(Cardinal cardinal) {
+		this.current = cardinal;
+	}
+
+	public Cardinal turnRight() {
+		// TODO
+		return null;
+	}
+
+	public Cardinal turnLeft() {
+		// TODO
+		return null;
+	}
 }
--- src/test/java/com/alvarolongueira/mowermanager/control/CardinalFactoryTest.java
+++ src/test/java/com/alvarolongueira/mowermanager/control/CardinalFactoryTest.java
@@ -1,36 +1,62 @@
 package com.alvarolongueira.mowermanager.control;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import com.alvarolongueira.mowermanager.domain.Cardinal;
+
 public class CardinalFactoryTest {
+
+	private final Cardinal cardinalNorth = Cardinal.N;
+	private final Cardinal cardinalSouth = Cardinal.S;
+	private final Cardinal cardinalWest = Cardinal.W;
+	private final Cardinal cardinalEast = Cardinal.E;
 
 	@Test
 	public void northToEastRight() {
 
+		Cardinal newCardinal = CardinalFactory.of(cardinalNorth).turnRight();
+
+		Assert.assertEquals(newCardinal, cardinalEast);
 	}
 
 	@Test
 	public void northToEastLeft() {
 
+		Cardinal newCardinal = CardinalFactory.of(cardinalNorth).turnRight();
+
+		Assert.assertEquals(newCardinal, cardinalEast);
 	}
 
 	@Test
 	public void southToWestRight() {
 
+		Cardinal newCardinal = CardinalFactory.of(cardinalNorth).turnRight();
+
+		Assert.assertEquals(newCardinal, cardinalWest);
 	}
 
 	@Test
 	public void southToWesttLeft() {
 
+		Cardinal newCardinal = CardinalFactory.of(cardinalNorth).turnRight();
+
+		Assert.assertEquals(newCardinal, cardinalWest);
 	}
 
 	@Test
 	public void northToSouthSeveralTurns() {
 
+		Cardinal newCardinal = CardinalFactory.of(cardinalNorth).turnRight();
+
+		Assert.assertEquals(newCardinal, cardinalSouth);
 	}
 
 	@Test
 	public void westToEastSeveralTurns() {
 
+		Cardinal newCardinal = CardinalFactory.of(cardinalNorth).turnRight();
+
+		Assert.assertEquals(newCardinal, cardinalWest);
 	}
 }
