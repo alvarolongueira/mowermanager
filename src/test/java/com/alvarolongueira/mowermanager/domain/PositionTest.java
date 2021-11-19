4fdee6a681f439fac917d696c35efc3a321f2fb4
Alvaro Longueira <alvarolco@ext.inditex.com>
2021-11-19 08:52:54 +0100

    Position test

---
+++ src/test/java/com/alvarolongueira/mowermanager/domain/PositionTest.java
@@ -0,0 +1,32 @@
+package com.alvarolongueira.mowermanager.domain;
+
+import org.junit.Test;
+
+public class PositionTest {
+
+	@Test
+	public void createPosition() {
+
+	}
+
+	@Test
+	public void movePositionOneNorth() {
+
+	}
+
+	@Test
+	public void movePositionOneSouth() {
+
+	}
+
+	@Test
+	public void movePositionOneEast() {
+
+	}
+
+	@Test
+	public void movePositionOneWest() {
+
+	}
+
+}
